aaa6fc798252e5e4946822fdfd884db7e85d767a
dmontoya1600 <78572569+dmontoya1600@users.noreply.github.com>
2021-10-07 11:25:45 -0500

    testing try excepts

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -23,13 +24,33 @@ password = driver.find_element_by_name('pass')
 password.send_keys(os.environ.get('PASSWORD'))
 
 password.send_keys(Keys.RETURN)
+# try:
+#     element = WebDriverWait(driver, 10).until(
+#         EC.presence_of_element_located((By.CLASS_NAME, 'oajrlxb2 rq0escxv'))
+#     )
+# finally:
+#     driver.get('https://www.facebook.com/pibelcalvario/')
+driver.implicitly_wait(10)
+driver.get('https://www.facebook.com/pibelcalvario/')
+print('ARE WE REACHING THIS?')
+# try:
+#     element = WebDriverWait(driver, 15).until(
+#         EC.presence_of_all_elements_located((By.CLASS_NAME, 'bi6gxh9e aov4n071'))
+#     )
+# finally:
+#     print('HIT THIS LINE')
+#     try:
+#         driver.find_elements_by_xpath("//*[contains(text(), ' was live.')]")
+#         print('FOUND ELEMENT')
+#     except NoSuchElementException:
+#         print('ELEMENT DOES NOT EXISTS')
+
+# child = driver.find_element_by_xpath("//*[contains(text(), ' was live.')]")
 try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, 'oajrlxb2 rq0escxv'))
-    )
+    driver.find_elements_by_xpath("//*[contains(text(), ' was live.')]")
+except NoSuchElementException:
+    print('ELEMENT DOES NOT EXISTS')
 finally:
-    driver.get('https://www.facebook.com/pibelcalvario/')
-
-
+    print('FOUND ELEMENT')
 
 # ON HEROKU
